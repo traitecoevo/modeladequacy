269d24e95aa59df5ffcdb41e62089da2594270cd
mwp <mwpennell@gmail.com>
2013-10-16 11:54:42 -0700

    added a function to read in leaf N data

--- analysis/R/read-data-functions.R
+++ analysis/R/read-data-functions.R
@@ -8,13 +8,18 @@ get.sla.data <- function(){
     t <- read.tree(file=file.path(getwd(), "data", "tempo_scrubbed_CONSTRAINT_rooted.dated.tre"))
     sla.raw <- read.csv(file=file.path(getwd(), "output", "species-mean-sla.csv"), header=TRUE)
 
-    ## natural log
+    ## only angiosperms
+    t <- extract.clade(t, node="Angiospermae")
+
+    ## base 10 log
     sla <- log10(sla.raw[,"x"])
     names(sla) <- sla.raw[,"X"]
 
     ## drop extra tips
     tmp <- t$tip.label[!t$tip.label %in% names(sla)]
     phy <- geiger:::.drop.tip(phy=t, tip = tmp)
+
+    sla <- sla[phy$tip.label]
 
     ## return tree and data
     list(phy=phy, states=sla, SE=0.1039405)
@@ -25,7 +30,11 @@ get.seedmass.data <- function(){
     t <- read.tree(file=file.path(getwd(), "data", "tempo_scrubbed_CONSTRAINT_rooted.dated.tre"))
     sm.raw <- read.csv(file=file.path(getwd(), "output", "species-mean-seedMass.csv"), header=TRUE)
 
-    ## natural log
+    ## only angiosperms
+    t <- extract.clade(t, node="Angiospermae")
+
+
+    ## base 10 log
     sm <- log10(sm.raw[,"x"])
     names(sm) <- sm.raw[,"X"]
 
@@ -56,3 +65,33 @@ get.seedmass.data <- function(){
     list(phy=phy, states=sm, SE=0.1551108)
 }
 
+
+
+
+
+get.leafn.data <- function(){
+    t <- read.tree("data/tempo_scrubbed_CONSTRAINT_rooted.dated.tre")
+    ln.raw <- read.csv("output/species-mean-leafN.csv")
+
+    ## only angiosperms
+    t <- extract.clade(t, node="Angiospermae")
+
+    ## log base 10
+    ln <- log10(ln.raw[,"x"])
+    names(ln) <- ln.raw[,"X"]
+
+    ## drop extra tips
+    tmp <- t$tip.label[!t$tip.label %in% names(ln)]
+    phy <- geiger:::.drop.tip(phy=t, tip = tmp)
+
+    ln <- ln[phy$tip.label]
+
+    ## return tree and data
+    list(phy=phy, states=ln, SE=0) ## need to update SE
+}
+
+    
+
+
+
+    
